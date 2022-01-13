aecd2a00799c5cf24e7d31e43cb75ca5d14284b4
Tech0tron <tech0tron.roblox@gmail.com>
2022-01-12 20:56:27 -0700

    Chapter 14 Chunks of bytecode

--- memory.h
+++ memory.h
@@ -7,12 +7,13 @@
 
 #include "common.h"
 
-#define GROW_CAPACITY(capacity) \
-    ((capacity) < 8 ? 8 : (capacity) * 2)
+#define GROW_CAPACITY(capacity) ((capacity) < 8 ? 8 : (capacity) * 2)
 
 #define GROW_ARRAY(type, pointer, oldCount, newCount) \
-    (type*)reallocate(pointer, sizeof(type) * (oldCount), sizeof(type) * (newCount) \
+    (type*)reallocate(pointer, sizeof(type) * (oldCount), sizeof(type) * (newCount))
+
+#define FREE_ARRAY(type, pointer, oldCount) \
+    reallocate(pointer, sizeof(type) * (oldCount), 0)
 
 void* reallocate(void* pointer, size_t oldSize, size_t newSize);
-// right b4 reallocate function
 #endif //CLOX_MEMORY_H
